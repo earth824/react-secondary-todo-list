3110dc44f278b00896c729bacddba7b5e01ad707
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-23 10:58:34 +0700

    fix component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import Pagination from './components/Pagination';
 import TodoContainer from './components/TodoContainer';
 import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
 
 function App() {
   return (
@@ -10,11 +8,6 @@ function App() {
         <TodoForm />
       </div>
       <TodoContainer />
-      <TodoList />
-      <div className="my-2 d-flex justify-content-between align-items-center">
-        <small className="text-muted">Showing 6 to 10 of 12 entries</small>
-        <Pagination />
-      </div>
     </div>
   );
 }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,6 +3,8 @@ import PageLimit from './PageLimit';
 import SearchStatus from './SearchStatus';
 import SearchText from './SearchText';
 import Sort from './Sort';
+import TodoList from './TodoList';
+import Pagination from './Pagination';
 
 function TodoContainer() {
   return (
@@ -14,6 +16,11 @@ function TodoContainer() {
       <div className="my-2 d-flex justify-content-between">
         <PageLimit />
         <Sort />
+      </div>
+      <TodoList />
+      <div className="my-2 d-flex justify-content-between align-items-center">
+        <small className="text-muted">Showing 6 to 10 of 12 entries</small>
+        <Pagination />
       </div>
     </Fragment>
   );
