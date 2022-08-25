aa33ab09bb990c5a7cb32544ec08b2ab568e6009
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-25 13:15:42 +0700

    complete lesson

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useEffect, useReducer, useState } from 'react';
+import axios from 'axios';
 import TodoContainer from './components/TodoContainer';
 import TodoForm from './components/TodoForm';
 
+function reducer(state, action) {
+  if (action.type === 'LOAD_TODO') {
+    return action.payload.todos;
+    // { type: 'CREATE_TODO', payload: { todo: new object todo } }
+  } else if (action.type === 'CREATE_TODO') {
+    return [action.payload.todo, ...state];
+  }
+}
+
 function App() {
+  // const [todos,setTodos] = useState([])
+  // const obj = useReducer(reducer, []); // return [currentState, dispatchFunction]
+  // dispatchFunction(actionObj) actionObj = { type }
+  // Todo WEB App มี Action ที่อาจจะเกิดขึ้น เช่น Create Todo, Delete Todo, Update Todo
+
+  const [todos, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/todos')
+      .then(res => {
+        dispatch({ type: 'LOAD_TODO', payload: { todos: res.data.todos } });
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const createTodo = title => {
+    axios
+      .post('http://localhost:8080/todos', { title, completed: false })
+      .then(res => {
+        dispatch({ type: 'CREATE_TODO', payload: { todo: res.data.todo } });
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="container mt-5 mb-3" style={{ maxWidth: 576 }}>
       <div className="my-4">
-        <TodoForm />
+        <TodoForm onSubmit={createTodo} />
       </div>
       <TodoContainer />
     </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,23 @@
-function TodoForm() {
+import { useState } from 'react';
+
+function TodoForm({ onSubmit }) {
+  const [input, setInput] = useState('');
+
+  const handleSubmitForm = e => {
+    e.preventDefault();
+    onSubmit(input);
+    setInput('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmitForm}>
       <div className="input-group">
-        <input type="text" className="form-control" />
+        <input
+          type="text"
+          className="form-control"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
         <button className="btn btn-primary">
           <i className="fa-solid fa-check" />
         </button>
