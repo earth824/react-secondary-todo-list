39deda8804774af7c894adfaed3a57442a8f4208
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-26 10:05:08 +0700

    add feature toggle status

--- src/components/TodoContent.js
+++ src/components/TodoContent.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { DELETE_TODO } from '../actions/todoAction';
+import { DELETE_TODO, UPDATE_TODO } from '../actions/todoAction';
 import axios from '../config/axios';
 import { TodoContext } from '../contexts/TodoContext';
 
@@ -14,13 +14,25 @@ function TodoContent({ todo: { title, completed, id }, openEdit }) {
       .catch(err => console.log(err));
   };
 
+  const handleClickToggle = () => {
+    axios
+      .put('/todos/' + id, { title, completed: !completed })
+      .then(res => {
+        ctx.dispatch({ type: UPDATE_TODO, payload: { todo: res.data.todo } });
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="d-flex align-items-center">
       <span className="flex-fill" role="button" onClick={openEdit}>
         {title}
       </span>
       <div className="btn-group">
-        <button className="btn btn-outline-secondary">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={handleClickToggle}
+        >
           <i className={`fa-solid fa-toggle-${completed ? 'on' : 'off'}`} />
         </button>
         <button
